101667242ae648b9512b80ad48fabc95c3bed027
Anco Chien <anco.chien@gmail.com>
2017-07-30 21:49:25 -0700

    updated readme and shopProducts function

---
+++ README.md
@@ -0,0 +1,11 @@
+#Bamazon
+
+An Amazon-like store front node applicaton using MYSQL
+
+
+###To Install:
+1. Git Clone the repository
+2. Navigate to the folder where the repository exists using Git Bash or Terminal
+3. Run the command npm install to download the required dependencies
+4. Set up your own database in MySQL Workbench using the schema and dummy values provided in bamazon.sql
+5. Then run the file on the CLI using node bamazonCustomer.js 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
-var fs = require('fs');
+// var fs = require('fs');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,3 +16,57 @@ connection.connect(function(err){
   console.log("Welcome to Bamazon!");
 
 });
+
+var shopProducts = function(){
+  connection.query("SELECT * FROM products", function(err, res){
+    //show list of products available for purchase
+    //push items into products array using for loop
+    inquirer.prompt({
+      name: "products",
+      type: "rawlist",
+      choices: function(value){
+        var productsArray = [];
+        for(var i = 0; i < res.length; i++){
+          productsArray.push(res[i].product_name);
+        }
+        return productsArray;
+      },
+      message: "What is the ID of the product you would you like to buy?"
+    }).then(function(answer){
+      for(var i = 0; i < res.length; i++){
+
+        if(res[i].product_name === answer.products) {
+          var chosenItem = res[i];
+          inquirer.prompt({
+            name: "quantity",
+            type: "input",
+            message: "How many units would you like to buy?"
+          }).then(function(answer){
+            console.log("The quantity I want is: " + answer.quantity);
+            console.log("This is the quantity available: " + chosenItem.stock_quantity);
+
+            if(chosenItem.stock_quantity < parseInt(answer.quantity)){
+              console.log("Sorry, we only have " + chosenItem.stock_quantity + " of those in stock.");
+              shopProducts();
+            } else {
+
+              connection.query("UPDATE products SET ? WHERE ?", [{
+                stock_quantity: chosenItem.stock_quantity - parseInt(answer.quantity)
+
+              }, {
+                item_id: chosenItem.item_id
+              }], function(err, res){
+                  var total = chosenItem.price * parseInt(answer.quantity);
+                  console.log("Your total is $" + total);
+                  shopProducts();
+
+              });
+
+            }
+          })
+        }
+      }
+    })
+  })
+}
+shopProducts();
